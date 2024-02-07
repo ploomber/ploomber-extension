10080ff88ff168e88ed0c53ba7cd772640908823
neelasha23 <neelasha.sen@gmail.com>
2024-02-07 16:05:19 +0530

    renamed title

--- ui-tests/jupyter_server_test_config.py
+++ ui-tests/jupyter_server_test_config.py
@@ -18,6 +18,6 @@ dot_ploomber = str(Path(c.ServerApp.root_dir, "dot-ploomber"))  # noqa: F821
 # c.ServerApp.log_level = "DEBUG"
 
 
-# patch the ploomber configurationd directory so it doesn't
+# patch the ploomber configuration directory, so it doesn't
 # interfere with the user's configuration
 telemetry.DEFAULT_HOME_DIR = dot_ploomber
